47a8151dfd05ef2acae2b65b3e1175cf011f7aee
ChrisGil <givc980909@gmail.com>
2023-01-03 14:09:25 -0600

    Implementación del servicio /v1/public/comics
    Servicio que filtar por nombre de un comic

--- api_marvel/client.py
+++ api_marvel/client.py
@@ -71,3 +71,48 @@ class APICharacterSearch:
         response = get(**kwargs)
         _check_response(response)
         return [self.result(row) for row in response.json().get("data").get("results")]
+
+
+class APIComicsSearch:
+    """ Busqueda de comics por filtro """
+
+    _endpoint: ClassVar[str] = f"{API_MARVEL_HOST}/v1/public/comics"
+    _client_version: ClassVar[float] = client_version
+
+    def __init__(self, **kwargs):
+        self.title_starts_with = kwargs.get("titleStartsWith", 'None')
+        self.title = kwargs.get("title", 'None')
+        self.query_params = {key: value for key, value in self._params.items() if value != 'None'}
+        self.response = self._get
+
+    @property
+    def _params(self) -> dict:
+        return {
+            "title": self.title,
+            "titleStartsWith": self.title_starts_with,
+            "orderBy": "title",
+            "apikey": PUB_KEY,
+            "hash": hash_key(1),
+            "ts": 1,
+        }
+
+    @property
+    def _get(self) -> dict[str, Any]:
+        return self._request(url=self._endpoint, params=self.query_params, headers=headers())
+
+    @staticmethod
+    def get_on_sale_date(row: dict) -> list:
+        return [row.get("date") for row in row.get("dates") if row.get("type") == "onsaleDate"]
+
+    def result(self, row: dict) -> dict:
+        return {
+            "id": row.get("id"),
+            "title": row.get("title"),
+            "image": row.get("thumbnail").get("path"),
+            "onSaleDate": self.get_on_sale_date(row)[0]
+        }
+
+    def _request(self, **kwargs) -> dict | list:
+        response = get(**kwargs)
+        _check_response(response)
+        return [self.result(row) for row in response.json().get("data").get("results")]
